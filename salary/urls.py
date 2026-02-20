3efd669f13203cceef3caefcc5afb4edd5c100e6
savithri98 <apjabdul96@gmail.com>
2026-02-20 11:27:46 +0530

    remove salary, payroll folder

--- salary/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import SalaryComponentViewSet, SalaryStructureViewSet
-
-router = DefaultRouter()
-router.register(r'salary-components', SalaryComponentViewSet)
-router.register(r'salary-structures', SalaryStructureViewSet)
-
-urlpatterns = router.urls
